0d63a40b027d4441a543c397d556b22e4e475c43
leona <leonardodasilva.ifrn@gmail.com>
2019-10-30 19:20:37 -0300

    ex002

---
+++ 020.solution/qq2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Pilha<ll>
+{
+    private List<ll> lista = new List<ll>();
+    public int Count()
+    {
+        return lista.Count;
+    }
+    public ll Pop()
+    {
+        ll a;
+        a = lista[Count()];
+        lista.RemoveAt(Count());
+        return a;
+    }
+    public void Push(ll obj)
+    {
+        lista.Insert(Count(), obj);
+    }
+    public ll Top()
+    {
+        ll a;
+        a = lista[Count()];
+        return a;
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("!!!");
+        Console.ReadKey();
+    }
+}
+
