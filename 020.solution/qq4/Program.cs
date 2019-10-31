e13f4c2dd14cacdede6b0f13fd1d1561e7bbfb78
leona <leonardodasilva.ifrn@gmail.com>
2019-10-30 21:53:20 -0300

    ex004

---
+++ 020.solution/qq4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+class Agenda
+{
+    private List<Contato> listc = new List<Contato>();
+    public void Inserir(Contato c)
+    {
+        listc.Add(c);
+    }
+    public List<Contato> Listar()
+    {
+        return listc;
+    }
+    public List<Contato> Pesquisar(string ini)
+    {
+        List<Contato> cd = new List<Contato>();
+        foreach (Contato x in listc)
+        {
+            if (x.Nome.IndexOf(ini) != -1) cd.Add(x);
+        }
+        return cd;
+    }
+   
+    public void Salvar()
+    {
+
+    }
+    public void Abrir()
+    {
+
+    }
+}
+class Contato
+{
+    public string Nome { set; get; }
+    public string Email { set; get; }
+    public string Fone { set; get; }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("!!!");
+        Console.ReadKey();
+    }
+}
